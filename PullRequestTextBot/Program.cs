6118a5cebc58e29a309d4ca7660854def6bcefa9
Robson de Moraes Silva <rms2307@outlook.com>
2024-08-18 00:23:25 -0300

    Update Program.cs

--- PullRequestTextBot/Program.cs
+++ PullRequestTextBot/Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("Starting App...");
+﻿using PullRequestTextBot;
 
+Console.WriteLine("Starting App...");
 
+string branch1 = "main";
+string branch2 = "develop";
+
+GitService gitService = new();
+string diffs = gitService.ExecuteGitCommandDiff(branch1, branch2);
+
+IAService iaService = new();
+string requestText = await iaService.GenerateTextPullRequest(diffs);
+
+Console.WriteLine("Finishing App...");
