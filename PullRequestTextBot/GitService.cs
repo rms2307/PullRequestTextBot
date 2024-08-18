e96112f74bcd13921b9d87dc80d9f029171ab1c7
Robson de Moraes Silva <rms2307@outlook.com>
2024-08-18 00:22:47 -0300

    Implementa GitService com a lógica para executar o comando diff

---
+++ PullRequestTextBot/GitService.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace PullRequestTextBot
+{
+    public class GitService
+    {
+        public string ExecuteGitCommandDiff(string source, string destination)
+        {
+            Process process = new();
+            process.StartInfo.FileName = "git";
+            process.StartInfo.Arguments = $"diff {source} {destination}";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+
+            process.WaitForExit();
+
+            return output;
+        }
+    }
+}
